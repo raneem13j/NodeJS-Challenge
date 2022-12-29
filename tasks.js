e193b31dcfa0463d04fedce375bf840f6a9e228e
Raneem <aljamal.raneem@gmail.com>
2022-12-29 15:32:04 +0200

    exit command

--- tasks.js
+++ tasks.js
@@ -34,7 +34,7 @@ function startApp(name){
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n') {
+  if (text === 'quit\n', text === 'exit\n') {
     quit();
   }
   else if(text === 'hello\n'){
